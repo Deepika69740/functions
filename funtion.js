28d528b472819d0e257112b06f8ba7c0d265f784
Deepika69740 <deepikareddyyannam6@gmail.com>
2024-12-10 20:11:35 +0530

    funtions

---
+++ funtion.js
@@ -0,0 +1,11 @@
+// Create a function where the function needs to have an object where obj consists of hero_name,movie_name,director_name,and plotof_movie and invoke the function and write a doc about that function working behind the scenes
+function obj(){
+    movie={
+        hero_name: "ntr",
+        movie_name: "devera",
+        director_name: "Rajamouli",
+        plotof_movie: "Netflix"
+    }
+    console.log(movie)
+}
+obj()
